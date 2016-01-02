bf3872838a75f67e7232e74c266d749b3f9b214e
tpdlzh <jackandiko@hotmail.com>
2016-01-02 16:36:56 +1100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,1 @@
-module.exports = {
-	db:'mongodb://tpdlzh:nam8315@ds035137.mongolab.com:35137/events',
-	sessionSecret:'development',
-	JWTSECRET:'development',
-	blockURL:["/verify_your_email"],
-	profileImageMimeType:["image/jpg","image/jpeg","image/png","image/gif"],
-	profileImageMaxSize:2000000
-}
+
